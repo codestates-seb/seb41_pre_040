8231a8c2fca2e0fe789b332a1d8e85247db2b830
aemaaeng <haemin120@naver.com>
2022-12-21 16:39:25 +0900

    client 초기 세팅

---
+++ Client/src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <body>안녕하세요.</body>
+    </div>
+  );
+}
+
+export default App;
