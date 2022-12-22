3637d63f8cfc90bd151ccf535580f68a48c4dbf9
aemaaeng <haemin120@naver.com>
2022-12-22 18:09:38 +0900

    Home 페이지 필터링버튼 제외 구현 완료

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,17 +1,16 @@
 import React from "react";
 import styled from "styled-components";
+import Question from "../components/Question";
 import { Link } from "react-router-dom";
 
 const MainContent = styled.div`
   width: calc(100% - 300px - 24px);
-  border: 1px solid green;
   padding: 24px;
 `;
 
 const TitleAndButton = styled.div`
   display: flex;
   align-items: center;
-  border: 1px solid blue;
   padding-bottom: 30px;
   h1 {
     font-size: 1.6rem;
@@ -22,18 +21,24 @@ const TitleAndButton = styled.div`
 `;
 
 const AskQuestion = styled(Link)`
-  background-color: #278bf7;
+  background-color: #0a95ff;
   color: white;
-  padding: 10px 12px;
+  font-weight: 555;
+  padding: 11px 11px;
   border-radius: 3px;
-  font-size: 0.9rem;
+  font-size: 0.8rem;
   &:hover {
     background-color: #126fd4;
   }
 `;
 
+const Questions = styled.div`
+  margin-left: -24px;
+  border-top: 1px solid #e3e6e8;
+`;
+
 const FilterButtons = styled.div`
-  border: 1px solid blue;
+  border-top: 1px solid blue;
 `;
 
 const Home = () => {
@@ -46,6 +51,17 @@ const Home = () => {
       <FilterButtons>
         <div>필터링 버튼 들어갈 자리</div>
       </FilterButtons>
+      {/* 일단은 하드코딩해놓기 */}
+      <Questions>
+        <Question />
+        <Question />
+        <Question />
+        <Question />
+        <Question />
+        <Question />
+        <Question />
+        <Question />
+      </Questions>
     </MainContent>
   );
 };
