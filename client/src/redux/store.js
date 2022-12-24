a9b191f9f7b55412e6194eca8c806b4e74b2bf09
KISOOJIN <ekf4455@naver.com>
2022-12-24 20:18:32 +0900

    로그인 & 로그아웃 기능 구현

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import questionsReducer from "./questionsSlice";
+import useReducer from "./user";
 
 export default configureStore({
   reducer: {
     questions: questionsReducer,
+    user: useReducer,
   },
 });
