4e1ba32429d68283c1415cde1e8a2f5c35e3cca4
jaehak24 <65396939+jaehak24@users.noreply.github.com>
2022-12-27 22:12:22 +0900

    auditing fields id 주석처리

--- Backend/pre-project-040/src/main/java/com/codestates/preproject040/service/HashtagService.java
+++ Backend/pre-project-040/src/main/java/com/codestates/preproject040/service/HashtagService.java
@@ -22,6 +22,10 @@ public class HashtagService {
         return list;
     }
 
+    // 리스르로 받았을 때는, 쪼개서 각 자 있는지 확인 ,없으면 신규 ID를 추가
+    // 없는 ID면 새로운 ID 발급
+
+    // 입력받은 해시태그 예시 {"가", "나", "다", "라"}
     public List<QuestionHashtag> createHashtag(QuestionDto questionDto) {
         //입력받은 List 값 가져오는 단계
         List<QuestionHashtag> list = questionDto.questionHashtag();
@@ -29,8 +33,8 @@ public class HashtagService {
         //for문으로 List순회
         for(QuestionHashtag tagName: list) {
             //레포에 있는지 확인할 차례
-            //레포에 해당하는 tagName 없으면(=처음 쓴 해시태그)
-            if (searchTagName(tagName).isEmpty()) {
+            //레포에 해당하는 tagName 없으면(=처음  쓴 해시태그)
+            if (searchTagName(name).isEmpty()) {
                 //레포에 저장
                 hashtagRepository.save(tag)
             }
