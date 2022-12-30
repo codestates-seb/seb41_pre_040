58a04c7fe8f3fe1091794a4914d821bd7950012f
aemaaeng <haemin120@naver.com>
2022-12-30 17:22:16 +0900

    서버 연동

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,12 +1,10 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
-  // 서버 주소: "http://3.36.232.23/"
-  // json-server 주소: "http://localhost:3001"
   app.use(
     "^/api/",
     createProxyMiddleware({
-      target: "http://localhost:3001",
+      target: "http://3.36.232.23:8080/",
       pathRewrite: { "^/api/": "/" },
       changeOrigin: true,
     })
