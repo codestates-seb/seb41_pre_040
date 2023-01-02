498ed2723fd8b4580e331fadca8b54200c00da56
KISOOJIN <ekf4455@naver.com>
2023-01-02 14:34:02 +0900

    store.js 수정

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -2,7 +2,6 @@ import { configureStore } from "@reduxjs/toolkit";
 import questionsReducer from "./questionsSlice";
 import usersReducer from "./usersSlice";
 import userReducer from "./user";
-import userInfoReducer from "./userInfo";
 import answersReducer from "./answersSlice";
 
 export default configureStore({
@@ -10,7 +9,6 @@ export default configureStore({
     questions: questionsReducer,
     users: usersReducer,
     user: userReducer,
-    userInfo: userInfoReducer,
     answers: answersReducer,
   },
 });
